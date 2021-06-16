5013d4fbca983a2e9297d5f70264d31083849d11
Edinson <evegab@unal.edu.co>
2021-06-16 08:51:21 -0500

    3 commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -24,36 +24,38 @@ public class ProductController {
 	private static final Logger LOG = (Logger) LoggerFactory.getLogger(ProductController.class);
 	 
     @Autowired
-    ProductService service;
+    //ProductService service;
  
     // Save a new product.
     @PostMapping
     public String save(@RequestBody final Product product) {
-        LOG.info("Saving the new product to the redis.");
-        service.save(product);
+        //LOG.info("Saving the new product to the redis.");
+        //service.save(product);
         return "Successfully added. PRODUCT with id= " + product.getId();
     }
  
     // Get all products
-    @GetMapping("/getall")
-    public Map<String, Product> findAll() {
+    @GetMapping()
+    public String findAll() {
         LOG.info("Fetching all products from the redis.");
-        final Map<String, Product> productMap = service.findAll();
-        return productMap;
+        //final Map<String, Product> productMap = service.findAll();
+        //return productMap;
+        return "lista de productos";
     }
  
     // Get product by id
     @GetMapping("/{id}")
-    public Product findById(@PathVariable("id") final String id) {
+    public String findById(@PathVariable("id") final String id) {
         LOG.info("Fetching product with id= " + id);
-        return service.findById(id);
+        //return service.findById(id);
+        return "product by id";
     }
     
     // update product by id
     @PutMapping("/{id}")
     public String update(@PathVariable("id") final String id, @RequestBody final Product product) {
         LOG.info("Deleting product with id= " + id);
-        service.updateById(id, product);
+        //service.updateById(id, product);
         return "Successfully added. product with id= " + product.getId();
     }
  
@@ -61,7 +63,7 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") final String id) {
         LOG.info("Deleting product with id= " + id);
-        service.delete(id);
+        //service.delete(id);
         return "product" + id + ", eliminado";
     }
     
